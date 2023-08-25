ae08ddace6e13bc195b824ae0431b329887e89ce
Reza Merzaie <ghrezam97@gmail.com>
2023-08-25 19:25:47 +0430

    Fix linter errors

--- enum.rb
+++ enum.rb
@@ -1,44 +1,43 @@
 # my_enumerable.rb
 module MyEnumerable
-    def all?
-      each do |element|
-        return false unless yield(element)
-      end
-      true
+  def all?
+    each do |element|
+      return false unless yield(element)
     end
-  
-    def any?
-      each do |element|
-        return true if yield(element)
-      end
-      false
-    end
-  
-    def filter
-      result = []
-      each do |element|
-        result << element if yield(element)
-      end
-      result
-    end
+    true
   end
-  
-  # my_list.rb
-  class MyList
-    include MyEnumerable
-    def initialize(*list)
-      @list = list
+
+  def any?
+    each do |element|
+      return true if yield(element)
     end
-    def each
-      @list.each do |element|
-        yield element
-      end
-    end
+    false
   end
-  # main.rb
-  list = MyList.new(1, 2, 3, 4)
-  puts list.all? { |e| e < 5 }
-  puts list.all? { |e| e > 5 }
-  puts list.any? { |e| e == 2 }
-  puts list.any? { |e| e == 5 }
-  puts list.filter { |e| e.even? }
+
+  def filter
+    result = []
+    each do |element|
+      result << element if yield(element)
+    end
+    result
+  end
+end
+
+# my_list.rb
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+# main.rb
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))
