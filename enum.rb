68cb1b22323b24e1dc1cab4e57b9c6cd9d4f262e
Reza Merzaie <ghrezam97@gmail.com>
2023-08-25 19:24:07 +0430

    Add the MyList class with its methods

--- enum.rb
+++ enum.rb
@@ -23,3 +23,22 @@ module MyEnumerable
     end
   end
   
+  # my_list.rb
+  class MyList
+    include MyEnumerable
+    def initialize(*list)
+      @list = list
+    end
+    def each
+      @list.each do |element|
+        yield element
+      end
+    end
+  end
+  # main.rb
+  list = MyList.new(1, 2, 3, 4)
+  puts list.all? { |e| e < 5 }
+  puts list.all? { |e| e > 5 }
+  puts list.any? { |e| e == 2 }
+  puts list.any? { |e| e == 5 }
+  puts list.filter { |e| e.even? }
