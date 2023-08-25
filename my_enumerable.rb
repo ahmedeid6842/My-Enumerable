5a7f77a83cabd9aa9b2bc3b8db608541f0f57e97
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-25 18:28:24 +0300

    Make each class and module has it's own file

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -22,22 +22,3 @@ module MyEnumerable
     result
   end
 end
-
-# my_list.rb
-class MyList
-  include MyEnumerable
-  def initialize(*list)
-    @list = list
-  end
-
-  def each(&block)
-    @list.each(&block)
-  end
-end
-# main.rb
-list = MyList.new(1, 2, 3, 4)
-puts(list.all? { |e| e < 5 })
-puts(list.all? { |e| e > 5 })
-puts(list.any? { |e| e == 2 })
-puts(list.any? { |e| e == 5 })
-puts(list.filter(&:even?))
