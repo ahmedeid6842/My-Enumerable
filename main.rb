031d317d5c7cff6370890dec4d38d4ba932a19e5
Reza Merzaie <ghrezam97@gmail.com>
2023-08-25 20:42:36 +0430

    Fixe the linters error

--- main.rb
+++ main.rb
@@ -1,10 +1,9 @@
 require_relative 'my_list'
 
-  # main.rb
-  list = MyList.new(1, 2, 3, 4)
-  puts(list.all? { |e| e < 5 })
-  puts(list.all? { |e| e > 5 })
-  puts(list.any? { |e| e == 2 })
-  puts(list.any? { |e| e == 5 })
-  puts(list.filter(&:even?))
-  
+# main.rb
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))
--- my_list.rb
+++ my_list.rb
@@ -2,12 +2,12 @@ require_relative 'my_enumerable'
 
 # my_list.rb
 class MyList
-    include MyEnumerable
-    def initialize(*list)
-      @list = list
-    end
-  
-    def each(&block)
-      @list.each(&block)
-    end
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
   end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
